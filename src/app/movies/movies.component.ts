83d8272ca7bee61b361ecc756610f1bd2bb10563
Fatikhan Gasimov <fatikhan.gasimov@gmail.com>
2020-03-25 13:52:22 -0400

    Generate component

---
+++ src/app/movies/movies.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'movies',  // <movies></movies>
+   templateUrl:'./movies.component.html',
+})
+
+export class MoviesComponent{
+
+}
